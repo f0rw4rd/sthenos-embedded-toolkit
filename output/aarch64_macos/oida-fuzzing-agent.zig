e754c2f37440f02553d7e188aa8446e3612d9dd9
f0rw4rd <26039322+f0rw4rd@users.noreply.github.com>
2026-06-15 18:30:56 +0200

    binaries: oida-fuzzing-agent (57 arches)

---
+++ output/aarch64_macos/oida-fuzzing-agent.zig
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d56c2d3e1cec8b41cd0a3d9e68b46973c7e2cc0a74c03547cbad8c8ade3e7f5
+size 58336
---
+++ output/x86_64_macos/oida-fuzzing-agent.zig
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:21f027b1682ea0d2ba69cb6bf0488ae7d5852e41f311865d842abd9c9988c3a6
+size 31747
